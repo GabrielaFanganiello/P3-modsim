61cb9b2d871e3e75453551e182a0cb6d03b4f8b9
Gabriela Fanganiello <gabrielafanganiello@gmail.com>
2023-05-08 09:24:08 -0600

    montando estrutura inicial do codigo

---
+++ Funções.py
@@ -0,0 +1,24 @@
+# Tema
+    # Pendulo elastico 2d
+
+
+# Dados experimentais
+
+
+# Simplificações feitas / abstração
+
+# Condições iniciais
+
+
+# Implementando função do modelo
+
+# Resolvendo as equações diferenciais por ODEINT
+
+#Plotando os gráficos
+
+# Validação
+
+# Gráfico conclusivo
+
+# Código das animações
+
